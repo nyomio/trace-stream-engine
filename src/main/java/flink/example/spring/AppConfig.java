0285e951ec98fc1cff18a1f1079244236be44d4d
Istvan Szoboszlai <istvan.szoboszlai@inepex.com>
2018-03-08 17:58:53 +0100

    Add forgotten file

---
+++ src/main/java/flink/example/spring/AppConfig.java
@@ -0,0 +1,10 @@
+package flink.example.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("flink.example")
+public class AppConfig {
+
+}
